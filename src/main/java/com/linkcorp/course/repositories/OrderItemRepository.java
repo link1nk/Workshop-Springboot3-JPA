b91e6bd24368dd586a44f9821378bb565a09c300
Lincoln Dias <lincolnroberto820@gmail.com>
2024-09-08 12:21:52 -0300

    OrderItem, many-to-many association with extra attributes - PART 2

---
+++ src/main/java/com/linkcorp/course/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.linkcorp.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.linkcorp.course.entities.OrderItem;
+import com.linkcorp.course.entities.pk.OrderItemPK;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
+
+}
